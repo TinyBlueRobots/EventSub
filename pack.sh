5e7fe87a274b84cf34c51734c2e9117eb17d39ad
Jon Canning <jon.canning@gmail.com>
2021-08-08 21:36:02 +0100

    update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 NUGETVERSION=1.0.0
 dotnet pack ./src/EventSub -c Release /p:PackageVersion=$NUGETVERSION
-dotnet nuget push ./src/EvenetSub/bin/Release/EventSub.$NUGETVERSION.nupkg -k $NUGETKEY -s nuget.org
+dotnet nuget push ./src/EventSub/bin/Release/EventSub.$NUGETVERSION.nupkg -k "$NUGETKEY" -s nuget.org
